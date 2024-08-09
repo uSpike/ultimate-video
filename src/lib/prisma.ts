988955b4f0737b53d6fc93d2c178be7b3aaa8567
Jordan Speicher <jordan@jspeicher.com>
2024-08-09 12:51:34 -0500

    Add handlePrismaError

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { error } from '@sveltejs/kit';
 
 const prisma = new PrismaClient();
 
 export default prisma;
+
+export function handlePrismaError(e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        error(400, e.message);
+    } else {
+        error(400, 'Unknown error');
+    }
+}
